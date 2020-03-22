01796e0e1cd8000b7590299a556127eee22673ad
mijaadela <adela.mija@yahoo.com>
2020-03-22 13:39:02 +0200

    TestGit.java

---
+++ TestGit.java
@@ -0,0 +1,9 @@
+package clase;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git ! Denumirea proiectului de licență este Sistem informatic pentru evidența contabilă");
+	}
+
+}
