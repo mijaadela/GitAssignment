e19e1bd7e11fc93f5a770c94dd7900a207f4c070
mijaadela <adela.mija@yahoo.com>
2020-03-22 13:47:03 +0200

    Al doilea commit: S-a adaugat clasa FacturaCumparare.

---
+++ FacturaCumparare.java
@@ -0,0 +1,12 @@
+package clase;
+
+public class FacturaCumparare {
+	protected String furnizor;
+	protected float valoareFactura;
+	
+	public FacturaCumparare(String furnizor, float valoareFactura) {
+		super();
+		this.furnizor = furnizor;
+		this.valoareFactura = valoareFactura;
+	}	
+}
